037355d0c30303b699b19f2359503816f830d0c1
Folia412 <79314786+Folia412@users.noreply.github.com>
2021-02-20 00:21:31 +0200

    Add new lines

--- test_of1_list.py
+++ test_of1_list.py
@@ -3,13 +3,16 @@ import unittest
 
 class TestListNumber(unittest.TestCase):
 
+
     def oftest1(self):
         self.assertIs(["j"], ["j"])
         self.assertIs(["-2"], ["-2"])
 
+
     def oftest2(self):
         self.assertIsNot(["k", "z", "l"], ["k", "l", "z"])
-        self.assertIsNot(["201"], ["201"])
+        self.assertIsNot(["201"], ["211"])
+
 
 if __name__ == '__main__':
     unittest.main()
